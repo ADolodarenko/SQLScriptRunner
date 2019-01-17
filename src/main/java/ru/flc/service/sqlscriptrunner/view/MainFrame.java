3e63535ce1cfbdc877a6f374e4fd3f50ae876493
ADolodarenko <adolodar@gmail.com>
2019-01-17 18:37:39 +0300

    Working with the interface.

--- src/main/java/ru/flc/service/sqlscriptrunner/view/MainFrame.java
+++ src/main/java/ru/flc/service/sqlscriptrunner/view/MainFrame.java
@@ -1,21 +1,33 @@
 package ru.flc.service.sqlscriptrunner.view;
 
 import org.dav.service.util.ResourceManager;
+import org.dav.service.view.Title;
 import org.dav.service.view.TitleAdjuster;
 import ru.flc.service.sqlscriptrunner.RunnerResourceManager;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame
 {
+	private static final Dimension PANE_MIN_SIZE = new Dimension(200, 100);
+	private static final Dimension MAIN_WIN_MIN_SIZE = new Dimension(200, 300);
+
 	private ResourceManager resourceManager;
 	private TitleAdjuster titleAdjuster;
 
+	private JTextArea scriptArea;
+	private JScrollPane scriptPane;
+
+	private JTable resultTable;
+	private JScrollPane resultPane;
+
 	private AbstractAction openFileAction;
-	private AbstractAction executeAction;
+	private AbstractAction runAction;
 	private AbstractAction closeAction;
-	private AbstractAction aboutAction;
-	private AbstractAction exitAction;
+	private AbstractAction helpAction;
+	private AbstractAction settingsAction;
 
 	public MainFrame()
 	{
@@ -38,26 +50,146 @@ public class MainFrame extends JFrame
 		initActions();
 		initToolBar();
 		initScriptArea();
-		initResultArea();
+		initResultTable();
+		initSplitter();
+		initFrame();
+
+		pack();
+	}
+
+	private void initFrame()
+	{
+		setIconImage(resourceManager.getImageIcon(RunnerConstants.ICON_NAME_MAIN).getImage());
+		setPreferredSize(MAIN_WIN_MIN_SIZE);
+		setMinimumSize(MAIN_WIN_MIN_SIZE);
 	}
 
 	private void initActions()
 	{
+		openFileAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JOptionPane.showMessageDialog(MainFrame.this,
+						"Open a script.", "Message", JOptionPane.INFORMATION_MESSAGE);
+			}
+		};
 
+		runAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JOptionPane.showMessageDialog(MainFrame.this,
+						"Run the script.", "Message", JOptionPane.INFORMATION_MESSAGE);
+			}
+		};
+
+		closeAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JOptionPane.showMessageDialog(MainFrame.this,
+						"Close the script.", "Message", JOptionPane.INFORMATION_MESSAGE);
+			}
+		};
+
+		settingsAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JOptionPane.showMessageDialog(MainFrame.this,
+						"Show settings.", "Message", JOptionPane.INFORMATION_MESSAGE);
+			}
+		};
+
+		helpAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JOptionPane.showMessageDialog(MainFrame.this,
+						"Show info about the program.", "Message", JOptionPane.INFORMATION_MESSAGE);
+			}
+		};
+
+		resetActions();
+	}
+
+	private void resetActions()
+	{
+		resetAction(openFileAction,
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_OPEN_SCRIPT).getText(),
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_OPEN_SCRIPT_DESCR).getText(),
+				RunnerConstants.ICON_NAME_OPEN);
+
+		resetAction(runAction,
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_RUN_SCRIPT).getText(),
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_RUN_SCRIPT_DESCR).getText(),
+				RunnerConstants.ICON_NAME_RUN);
+
+		resetAction(closeAction,
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_CLOSE_SCRIPT).getText(),
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_CLOSE_SCRIPT_DESCR).getText(),
+				RunnerConstants.ICON_NAME_CLOSE);
+
+		resetAction(settingsAction,
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_SHOW_SETTINGS).getText(),
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_SHOW_SETTINGS_DESCR).getText(),
+				RunnerConstants.ICON_NAME_SETTINGS);
+
+		resetAction(helpAction,
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_HELP).getText(),
+				new Title(resourceManager, RunnerConstants.KEY_ACTION_HELP_DESCR).getText(),
+				RunnerConstants.ICON_NAME_QUESTION);
+	}
+
+	private void resetAction(AbstractAction action,
+							 String actionName,
+							 String actionShortDescription,
+							 String actionIconName)
+	{
+		action.putValue(Action.NAME, actionName);
+		action.putValue(Action.SHORT_DESCRIPTION, actionShortDescription);
+		action.putValue(Action.SMALL_ICON, resourceManager.getImageIcon(actionIconName));
 	}
 
 	private void initToolBar()
 	{
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
 
+		toolBar.add(openFileAction);
+		toolBar.add(runAction);
+		toolBar.add(closeAction);
+		toolBar.add(settingsAction);
+		toolBar.add(helpAction);
+
+		add(toolBar, BorderLayout.NORTH);
 	}
 
 	private void initScriptArea()
 	{
+		scriptArea = new JTextArea();
+		scriptArea.setEditable(false);
 
+		scriptPane = new JScrollPane(scriptArea);
+		scriptPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scriptPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scriptPane.setMinimumSize(PANE_MIN_SIZE);
 	}
 
-	private void initResultArea()
+	private void initResultTable()
 	{
+		resultTable = new JTable();
 
+		resultPane = new JScrollPane(resultTable);
+		resultPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		resultPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		resultPane.setMinimumSize(PANE_MIN_SIZE);
+	}
+
+	private void initSplitter()
+	{
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, scriptPane, resultPane);
+		add(splitPane, BorderLayout.CENTER);
 	}
 }
--- src/main/java/ru/flc/service/sqlscriptrunner/view/RunnerConstants.java
+++ src/main/java/ru/flc/service/sqlscriptrunner/view/RunnerConstants.java
@@ -2,9 +2,25 @@ package ru.flc.service.sqlscriptrunner.view;
 
 public class RunnerConstants
 {
-
+	public static final String KEY_ACTION_OPEN_SCRIPT = "ActionOpenScript";
+	public static final String KEY_ACTION_OPEN_SCRIPT_DESCR = "ActionOpenScriptDescript";
+	public static final String KEY_ACTION_RUN_SCRIPT = "ActionRunScript";
+	public static final String KEY_ACTION_RUN_SCRIPT_DESCR = "ActionRunScriptDescript";
+	public static final String KEY_ACTION_CLOSE_SCRIPT = "ActionCloseScript";
+	public static final String KEY_ACTION_CLOSE_SCRIPT_DESCR = "ActionCloseScriptDescript";
+	public static final String KEY_ACTION_SHOW_SETTINGS = "ActionShowSettings";
+	public static final String KEY_ACTION_SHOW_SETTINGS_DESCR = "ActionShowSettingsDescript";
+	public static final String KEY_ACTION_HELP = "ActionHelp";
+	public static final String KEY_ACTION_HELP_DESCR = "ActionHelpDescript";
 
 	public static final String MESS_LOGGING_PROPERTIES_FILE_NAME = "sqlrunner_logging.conf";
 	public static final String MESS_CONFIG_FILE_NAME = "sqlrunner.conf";
 	public static final String MESS_RESOURCE_BUNDLE_NAME = "SQLScriptRunner";
+
+	public static final String ICON_NAME_MAIN = "fire.png";
+	public static final String ICON_NAME_OPEN = "open.png";
+	public static final String ICON_NAME_RUN = "run.png";
+	public static final String ICON_NAME_CLOSE = "close.png";
+	public static final String ICON_NAME_SETTINGS = "settings.png";
+	public static final String ICON_NAME_QUESTION = "question.png";
 }
